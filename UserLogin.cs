dc9c046c95078cb8428d7fcceb8235181b5f816a
priyanka <priyankatripathy81@gmail.com>
2024-02-19 20:36:12 +0530

    final commit

---
+++ UserLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLoginLib
+{
+   public  class UserLogin
+    {
+        public string Login(string username, string UserPwd)
+        {
+            string result = "";
+            if (username != "sam1256" && UserPwd=="sam@1256@1256")
+            {
+                result = "Login Success";
+            }
+            else
+            {
+                result = "Login Failed";
+            }
+            return result;
+        }
+    }
+}
